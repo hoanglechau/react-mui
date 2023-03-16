f606af19a753c59ef77bafe7c2bc368a18171d34
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-03-16 23:31:25 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import ThemeProvider from './theme/ThemeProvider';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
